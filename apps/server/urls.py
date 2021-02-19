f3e35208a6c3005e8d17f088a306c2b614f072c7
XantisS0 <patryk.makowski23@gmail.com>
2021-02-19 20:12:26 +0100

    add nested get and post requests

--- apps/server/urls.py
+++ apps/server/urls.py
@@ -1,17 +1,12 @@
-# from django.urls import include, path
-# from rest_framework_nested import routers
-# from rest_framework_nested import routers
+from django.urls import include, path
+from rest_framework import routers
 
-# from .views import ServersViewSet, ServerUsersViewSet
+from .views import ServersViewSet
 
 
-# router = routers.SimpleRouter()
-# router.register(r'', ServersViewSet)
+router = routers.SimpleRouter()
+router.register('', ServersViewSet)
 
-# users_router = routers.NestedSimpleRouter(router, r'', lookup='user')
-# users_router.register(r'users', ServerUsersViewSet, basename='server_users')
-
-# urlpatterns = [
-#     path('', include(router.urls)),
-#     # path('', include(users_router))
-# ]
+urlpatterns = [
+    path('', include(router.urls)),
+]
--- apps/server/views.py
+++ apps/server/views.py
@@ -1,27 +1,31 @@
-# from rest_framework import serializers, viewsets
-# from rest_framework.response import Response
+from rest_framework import serializers, viewsets
+from rest_framework.response import Response
 
-# from .models import Server
-# from ..user.models import User
-# from ..user.views import UserSerializer
-
-
-# class ServerSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Server
-#         fields = '__all__'
+from .models import Server
+from apps.user.models import User
 
 
-# class ServersViewSet(viewsets.ModelViewSet):
-#     serializer_class = ServerSerializer
-#     queryset = Server.objects.all()
+class ServerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Server
+        fields = '__all__'
+        depth = 1
 
 
-# class ServerUsersViewSet(viewsets.ViewSet):
-#     serializer_class = UserSerializer
-#     queryset = Server.objects.all()
+class ServersViewSet(viewsets.ModelViewSet):
+    serializer_class = ServerSerializer
+    queryset = Server.objects.all()
 
-#     def list(self, request, server_id):
-#         users = User.objects.filter(server=server_id)
-#         serializer = self.get_serializer(users, many=True)
-#         return Response(serializer.data)
+    def create(self, request, *args, **kwargs):
+        data = request.data
+
+        new_server = Server.objects.create(name = data['name'], logging = data['logging'])
+        new_server.save()
+
+        for user in data['users']:
+            user_obj = User.objects.get(username = user['username'])
+            new_server.users.add(user_obj)
+
+        serializer = ServerSerializer
+            
+        return Response(serializer.data)
