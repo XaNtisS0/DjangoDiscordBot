d18b5d45a90e6fd2487589c6c6de003497f64899
XantisS0 <patryk.makowski23@gmail.com>
2021-02-19 20:04:55 +0100

    add admin fields

--- apps/server/admin.py
+++ apps/server/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from apps.server.models import Server
 
-admin.site.register(Server)
+class ServerAdmin(admin.ModelAdmin):
+    list_display = ['name', 'logging']
+
+admin.site.register(Server, ServerAdmin)
--- apps/user/admin.py
+++ apps/user/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from apps.user.models import User
 
-admin.site.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username']
+
+admin.site.register(User, UserAdmin)
