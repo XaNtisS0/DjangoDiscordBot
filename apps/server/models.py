72780ec895cbd6962ed453b3bf2d40f395a48fe8
XantisS0 <patryk.makowski23@gmail.com>
2021-02-19 20:04:32 +0100

    updated models

--- apps/server/models.py
+++ apps/server/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
+import apps.user.models
+
 
 class Server(models.Model):
     name = models.CharField(max_length=200)
     logging = models.BooleanField(default=True)
+    users = models.ManyToManyField(apps.user.models.User)
 
     def __str__(self):
         return self.name
--- apps/user/models.py
+++ apps/user/models.py
@@ -4,8 +4,6 @@ import apps.rank.models
 
 class User(models.Model):
     username = models.CharField(max_length=200)
-    # server that this user is assigned to
-    server = models.ForeignKey(apps.server.models.Server, on_delete=models.CASCADE)
     ranks = models.ManyToManyField(apps.rank.models.Rank)
 
     def __str__(self):
